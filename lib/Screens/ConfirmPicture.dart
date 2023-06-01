abf6a55f384e2a18fcb79bf9a0e97b424baedd99
Anish <anish043014@gmail.com>
2023-06-01 19:15:13 +0400

    Initial commit
    Mo Nature _ver 1.1
    
    1. Bug fixed whereby the image on the Result Screen would go out of bounds when too big to fit together with text on the screen.
    
      1.1 Image in now made to fit inside a pre-sized Container widget before displaying.
    
    2. Added new animal species.
    
       2.1. This version can classify 3 plants and 5 animals endemic to Mauritius and 3 animals endemic to Rodrigues.
    
       2.2. Identifiable Classes are: 0 Trochetia Boutoniana
                                      1 Latanier Bleu
                                      2 Palmiste Bouteille
                                      3 Mauritius Kestrel
                                      4 Mauritius Echo Parakeet
                                      5 Pink Pigeon
                                      6 Mauritius Ornate Day Gecko
                                      7 Cardinal De Maurice
                                      8 Mascarene Paradise Flycatcher
                                      9 Mauritius Cuckooshrike
                                      10 Mauritius Olive-White Eye
                                      11 Rodrigues Flying Fox
                                       (Pteropus Rodricensis)
                                      12 Rodrigues Fody
                                      13 Rodrigues Warbler
    
       2.3 New added Endemic species for Mauritius are:
          - Mascarene Paradise Flycatcher
          - Mauritius Cuckooshrike
          - Mauritius Olive-White Eye
    
       2.4 New added Endemic species for Rodrigues are:
          - Rodrigues Flying Fox (Pteropus Rodricensis)
          - Rodrigues Fody
          - Rodrigues Warbler
    
    3. Confidence Level is now outputted in percentage to 3 Significant Figures.

--- lib/Screens/ConfirmPicture.dart
+++ lib/Screens/ConfirmPicture.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
-import 'package:http/http.dart' as http;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
